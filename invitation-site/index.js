cce2fdaa3f27c0e8c55eafa25f69fb2924175a22
Julie Munn <jmunn302@gmail.com>
2018-11-28 00:14:43 +0000

    automatically backed up by learn

--- invitation-site/index.js
+++ invitation-site/index.js
@@ -62,7 +62,9 @@ function changeEnvelope() {
     } else if (document.getElementById("lightblue").checked) { 
     document.getElementById("background").style.backgroundImage = "url('https://i.imgur.com/Tvqg0I3.jpg')";
     } else if (document.getElementById("blush").checked) { 
-    document.getElementById("background").style.backgroundImage = "url('https://i.imgur.com/62CkM2D.png')";
+    document.getElementById("background").style.backgroundImage = "url('https://i.imgur.com/WjU1QnZ.png')";
+    } else if (document.getElementById("silver").checked) { 
+    document.getElementById("background").style.backgroundImage = "url('https://i.imgur.com/WjU1QnZ.png')";
     }
 }
 
